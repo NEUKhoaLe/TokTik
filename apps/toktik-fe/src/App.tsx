3c6ee1b7ce6ec092f1b8d589c9852dd8fe015b06
Dang Khoa Le <khoale@Dangs-MacBook-Pro.local>
2024-03-21 00:10:36 -0400

    initial commit

---
+++ apps/toktik-fe/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Banner } from "common";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <Banner />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ shared/common/src/Banner.tsx
@@ -0,0 +1,3 @@
+export const Banner = function ({ instanceName = "test" }) {
+  return <h1>Welcome to the {instanceName} portal</h1>;
+};
